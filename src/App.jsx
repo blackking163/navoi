6bc47ab0a483315470a96ccc821693e5043c72ea
blackking163 <maikaljordan2323@gmail.com>
2024-12-25 19:20:28 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './assets/components/Hero/Hero'
+import Hiro from './assets/Hiro/Hiro'
+
+const App = () => {
+  return (
+   <>
+    <Hero/>
+    <Hiro/>
+   </>
+  )
+}
+
+export default App
---
+++ src/assets/Hiro/Hiro.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./Hiro.scss"
+
+const Hiro = () => {
+  return (
+  
+    <>
+     <section className="section">
+
+<div className="title">
+    <h1>СТАНЦИЯ НАВОИ </h1>
+</div>
+<div className="container">
+<div className="wrapper">
+
+
+    <div className="text">
+        <p>«Алишера Навои» (узб. Alisher Navoiy) — станция Ташкентского метрополитена.
+
+Открыта 8 декабря 1984 года в составе первого участка Узбекистанской линии «Алишера Навои» — «Ташкент».
+
+Расположена между станциями «Гафура Гуляма» и «Узбекистанская»[1].</p>
+    </div>
+
+    <div className="photo">
+        <img  src="/navoi.webp" alt="" />
+    </div>
+
+
+
+</div>
+</div>
+
+</section>
+    </>
+  )
+}
+
+export default Hiro
